8f87d872a48e2981ee41f51bbe844f7fb3885747
151250129 <151250129@smail.nju.edu.cn>
2017-05-07 20:41:03 +0800

    language changes

---
+++ Trillionaire/web/WEB-INF/lib/language_cookie.js
@@ -0,0 +1,49 @@
+var name = "somoveLanguage";
+
+function chgLang() {
+    var value = $("#ddlSomoveLanguage").children('option:selected').val();
+    SetCookie(name, value);
+    location.reload();
+}
+function SetCookie(name, value) {
+    var Days = 30; //此 cookie 将被保存 30 天
+    var exp = new Date();    //new Date("December 31, 9998");
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + encodeURI(value) + ";expires=" + exp.toGMTString();
+}
+
+function getCookie(name){    //取cookies函数
+    var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr !== null) return decodeURI(arr[2]);
+    return null
+}
+
+$(function () {
+
+    var brlanguage = (navigator.language || navigator.browserLanguage).toLowerCase();
+
+    if (brlanguage.indexOf("en") > -1) {
+
+        $("[data-localize]").localize("text", {language: "en"});
+    }
+    else if (brlanguage.indexOf("ja") > -1) {
+
+        $("[data-localize]").localize("text", {language: "ja"});
+
+    } else {
+
+        $("[data-localize]").localize("text", {language: "en"});
+    }
+    if (getCookie(name) !== "") {
+        if (getCookie(name) === "ja") {
+            $("[data-localize]").localize("text", {language: "ja"});
+        }
+        if (getCookie(name) === "en") {
+            $("[data-localize]").localize("text", {language: "en"});
+
+        }
+
+    }
+    //根据cookie选择语言
+
+});
--- Trillionaire/web/index.jsp
+++ Trillionaire/web/index.jsp
@@ -6,11 +6,28 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Language</title>
+    <script src="WEB-INF/lib/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="WEB-INF/lib/jquery.localize.js" type="text/javascript" charset="utf-8"></script>
+    <script src="WEB-INF/lib/language_cookie.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+<div class="top_lan">
+    <select id="ddlSomoveLanguage" onchange="chgLang();">
+        <option value="en">ENGLISH</option>
+        <option value="ja">日本語</option>
+    </select>
+</div>
+<div id="prod_navright">
+    <ul>
+        <li><a data-localize="hpt.management"></a></li>
+        <li><a data-localize="hpt.support"></a></li>
+        <li><a data-localize="hpt.tutorial"></a></li>
+        <li><a data-localize="hpt.features"></a></li>
+    </ul>
+</div>
+</body>
 </html>
