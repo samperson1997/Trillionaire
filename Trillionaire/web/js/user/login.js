aaa86e85360b54b6b3803778d580cdfc63b5bea4
151250128 <151250128@smail.nju.edu.cn>
2017-05-17 17:06:09 +0800

    szs 界面优化

--- Trillionaire/web/js/user/login.js
+++ Trillionaire/web/js/user/login.js
@@ -1,3 +1,5 @@
+var tip = document.getElementById('log-tip');
+
 angular.module("mainapp", [])
     .controller("UserController", function ($scope) {
         $scope.inputUsername = "";
@@ -5,27 +7,32 @@ angular.module("mainapp", [])
         //登录
         $scope.login = function () {
             if (checkFirst() != false) {
+                tip.innerHTML = "";
                 login_ajax($scope.inputUsername, $scope.inputPassword);
             } else {
-                alert("请将信息填写完整...");
-            }
-            ;
+                tip.innerHTML = "请把信息填写完整";
+            };
         };
+
         function checkFirst() {
-            if ($scope.inputUsername != null && $scope.inputUsername != ""
-                && $scope.inputPassword != null && $scope.inputPassword != "") {
+            if ($scope.inputUsername != null && $scope.inputUsername != "" &&
+                $scope.inputPassword != null && $scope.inputPassword != "") {
                 return true;
             } else {
                 return false;
             }
         };
+
         function login_ajax(username, password) {
             this.username = username;
             this.password = password;
             $.ajax({
                 type: "POST",
                 url: "/user/login",
-                data: {"username": this.username, "password": this.password},
+                data: {
+                    "username": this.username,
+                    "password": this.password
+                },
                 contentType: "application/x-www-form-urlencoded",
                 dataType: "text",
                 success: function (data) {
@@ -34,16 +41,16 @@ angular.module("mainapp", [])
                         var str = document.referrer;
                         if (data == "success") {
                             alert(str);
-                            window.sessionStorage.setItem("username",username);
-                            if (str == null||str=="") {
+                            window.sessionStorage.setItem("username", username);
+                            if (str == null || str == "") {
                                 window.location.href = "/index.html";
-                            }else{
-                                window.location.href =str;
+                            } else {
+                                window.location.href = str;
                             }
                         }
                         if (data == "fail") {
                             $scope.inputPassword = "";
-                            alert("用户名不存在或密码错误");
+                            tip.innerHTML = "用户名不存在或密码错误";
                         }
                         /*
                          if(data.success == true && data.message == "登录成功"){
@@ -63,4 +70,4 @@ angular.module("mainapp", [])
                 }
             });
         };
-    })  
+    })
--- Trillionaire/web/js/user/register.js
+++ Trillionaire/web/js/user/register.js
@@ -1,47 +1,57 @@
-angular.module("mainapp",[])  
-.controller("RegisterController",function($scope){
-    $scope.inputUsername = "";  
-    $scope.inputPassword = "";  
-    $scope.inputEmail = "";  
-    $scope.inputTel = "";  
-    function checkFirst(){  
-        if($scope.inputUsername!=null && $scope.inputUsername!=""  
-            && $scope.inputPassword!=null && $scope.inputPassword!=""  
-            && $scope.inputEmail!=null && $scope.inputEmail!=""  
-            && $scope.inputTel!=null && $scope.inputTel!=""){  
-            return true;  
-    }else{  
-        return false;  
-    }  
-};  
+var tip = document.getElementById('reg-tip');
+
+angular.module("mainapp", [])
+    .controller("RegisterController", function ($scope) {
+        $scope.inputUsername = "";
+        $scope.inputPassword = "";
+        $scope.inputPassword2 = "";
+
+        function checkFirst() {
+            if ($scope.inputUsername != null && $scope.inputUsername != "" &&
+                $scope.inputPassword != null && $scope.inputPassword != "" &&
+                $scope.inputPassword2 != null && $scope.inputPassword2 != "") {
+                return true;
+            } else {
+                return false;
+            }
+        };
         //注册  
-        $scope.register = function(){  
-            if(checkFirst() != false){  
-                $scope.inputPassword = hex_md5($scope.inputPassword);  
-                $.ajax({  
-                    type:"POST",
-                    url:"/user/register",
-                    data:{"username":$scope.inputUsername,"password":$scope.inputPassword,"tel":$scope.inputTel,"email":$scope.inputEmail},  
-                    contentType:"application/x-www-form-urlencoded",  
-                    dataType:"json",  
-                    success:function(data){  
-                        console.log(data);  
-                        $scope.$apply(function(){  
-                            if(data.success == true && data.message == "注册成功"){  
-                                $scope.inputUsername = "";$scope.inputPassword = "";  
-                                $scope.inputEmail = "";$scope.inputTel = "";  
-                                alert("注册成功!");  
+        $scope.register = function () {
+            if (checkFirst() != false) {
+                $scope.inputPassword = hex_md5($scope.inputPassword);
+                $.ajax({
+                    type: "POST",
+                    url: "/user/register",
+                    data: {
+                        "username": $scope.inputUsername,
+                        "password": $scope.inputPassword,
+                        "tel": $scope.inputTel,
+                        "email": $scope.inputEmail
+                    },
+                    contentType: "application/x-www-form-urlencoded",
+                    dataType: "json",
+                    success: function (data) {
+                        console.log(data);
+                        $scope.$apply(function () {
+                            if (data.success == true && data.message == "注册成功") {
+                                $scope.inputUsername = "";
+                                $scope.inputPassword = "";
+                                $scope.inputEmail = "";
+                                $scope.inputTel = "";
+                                tip.innerHTML = "欢迎,注册成功";
                                 window.location.href = "../../login.html";
-                            }else if(data.success == false && data.message == "该用户名已存在..."){  
-                                $scope.inputUsername = "";$scope.inputPassword = "";  
-                                $scope.inputEmail = "";$scope.inputTel = "";  
-                                alert("该用户名已被注册...");  
-                            }  
-                        });  
-                    }  
-                });  
-            }else{  
-                alert("请将信息填写完整...");  
-            };  
-        };  
-    })  
+                            } else if (data.success == false && data.message == "该用户名已存在...") {
+                                $scope.inputUsername = "";
+                                $scope.inputPassword = "";
+                                $scope.inputEmail = "";
+                                $scope.inputTel = "";
+                                tip.innerHTML = "该用户名已被注册";
+                            }
+                        });
+                    }
+                });
+            } else {
+                tip.innerHTML = "请把信息填写完整";
+            };
+        };
+    })
