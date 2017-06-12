bf080b723d293e976f53427e4766ccefdf59f123
151250128 <151250128@smail.nju.edu.cn>
2017-06-13 00:12:21 +0800

    szs 策略回测"参数调优"功能

--- Trillionaire/web/js/charts/win-line.js
+++ Trillionaire/web/js/charts/win-line.js
@@ -1,74 +1,71 @@
-var base = +new Date(1968, 9, 3);
-var oneDay = 24 * 3600 * 1000;
-var date = [];
-
-var data = [Math.random() * 300];
-
-for (var i = 1; i < 20000; i++) {
-    var now = new Date(base += oneDay);
-    date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
-    data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
-}
-
-option = {
-    tooltip: {
-        trigger: 'axis',
-        position: function (pt) {
-            return [pt[0], '10%'];
-        }
-    },
-    xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: date
-    },
-    yAxis: {
-        type: 'value',
-        min: 'dataMin',
-        max: 'dataMax',
-        boundaryGap: [0, '100%']
-    },
-    dataZoom: [
-        {
-            type: 'inside',
-            start: 50,
-            end: 100
+function getWinOP(plist, wlist) {
+    option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross'
+            },
+            backgroundColor: 'rgba(245, 245, 245, 0.8)',
+            borderWidth: 1,
+            borderColor: '#ccc',
+            padding: 10,
+            textStyle: {
+                color: '#000'
+            },
+            extraCssText: 'width: 170px'
         },
-        {
-            show: true,
-            type: 'slider',
-            y: '90%',
-            start: 50,
-            end: 100
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: plist
+        },
+        yAxis: {
+            type: 'value',
+            //            min: 'dataMin',
+            //            max: 'dataMax',
+            boundaryGap: [0, '100%']
+        },
+        dataZoom: [
+            {
+                type: 'inside',
+                start: 50,
+                end: 100
+        },
+            {
+                show: true,
+                type: 'slider',
+                y: '90%',
+                start: 50,
+                end: 100
         }
     ],
-    series: [
-        {
-            name: '模拟数据',
-            type: 'line',
-            smooth: true,
-            symbol: 'none',
-            sampling: 'average',
-            itemStyle: {
-                normal: {
-                    color: 'rgb(255, 70, 131)'
-                }
-            },
-            areaStyle: {
-                normal: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                        offset: 0,
-                        color: 'rgb(255, 158, 68)'
-                    }, {
-                        offset: 1,
+        series: [
+            {
+                name: '策略胜率',
+                type: 'line',
+                smooth: true,
+                symbol: 'none',
+                sampling: 'average',
+                itemStyle: {
+                    normal: {
                         color: 'rgb(255, 70, 131)'
+                    }
+                },
+                areaStyle: {
+                    normal: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                            offset: 0,
+                            color: 'rgb(255, 158, 68)'
+                    }, {
+                            offset: 1,
+                            color: 'rgb(255, 70, 131)'
                     }])
-                }
-            },
-            data: data
+                    }
+                },
+                data: wlist
         }
     ]
-};
+    };
 
-var winChart = echarts.init(document.getElementById('win-chart'));
-winChart.setOption(option);
+    return option;
+}
