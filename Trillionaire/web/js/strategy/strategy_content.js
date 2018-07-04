d8092a5b60ee4bb10b6800053fc4edc5f841104f
151250128 <151250128@smail.nju.edu.cn>
2018-07-04 23:41:36 +0800

    关注股票列表界面和策略列表界面修改

--- Trillionaire/web/js/strategy/strategy_content.js
+++ Trillionaire/web/js/strategy/strategy_content.js
@@ -1,5 +1,12 @@
 function loadStraList() {
-    $("#stra-table").html("<tr><th>名称</th><th>最新修改时间</th><th>操作</th></tr>");
+    $("#stra-table").html("<caption><p>策略列表</p>" +
+        "<a href=\"strategy-edit.html?sid=-1\">\n" +
+        "                <div class=\"stra-button\"><i class='fa fa-lightbulb-o'></i> 创建新策略</div>\n" +
+        "            </a>" +
+        "<a href=\"api.html\">\n" +
+        "                <div class=\"stra-button\"><i class=\"fa fa-book\"></i> API文档</div>\n" +
+        "            </a>" +
+        "</caption><tr><th>策略</th><th>操作</th><th>最新修改时间</th></tr>");
     var load = $.ajax({
         type: "GET",
         url: "/backtest/get_strategy_list",
@@ -10,12 +17,18 @@ function loadStraList() {
         dataType: "json",
         success: function (data0) {
             $.each(data0.nameList, function (i, value) {
-                $("#stra-table").append("<tr><td><a href=\"strategy-edit.html?sid=" + value.sid + "\">" + value.strategName + "</a></td><td>" + value.updateTime + "</td><td><a href=\"strategy-edit.html?sid=" + value.sid + "\"><div class=\"button-green\"><i class=\"fa fa-eye\"></i> 查看</div></a><div class=\"button-red\" id=\"" + value.sid + "delete-button\" onclick=\"deleteStra(this)\"><i class=\"fa fa-close\"></i> 删除</div></td></tr>");
+                $("#stra-table").append("<tr><td><a href=\"strategy-edit.html?sid=" + value.sid + "\">"
+                    + value.strategName + "</a><td>" + "<a href=\"strategy-edit.html?sid=" + value.sid + "\">" +
+                    "<div class=\"stra-delete-button\">" +
+                    "<i class='fa fa-eye'></i> 详情</div></a> | " +
+                    "<div class=\"stra-delete-button\" id=\"" + value.sid + "delete-button\" onclick=\"deleteStra(this)\">" +
+                    "<i class=\"fa fa-trash\"></i> 删除</div></td></td><td>" + value.updateTime.substr(0, 16)
+                    + "</tr>");
                 sessionStorage.setItem(value.strategName, value.sid);
             })
         },
         error: function (request, status, err) {
-            if (status == "timeout") {
+            if (status === "timeout") {
                 load.abort();
             }
         }
